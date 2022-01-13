a019d2e2f54343bec18550c44e829682af026e1a
Guilherme Taira <guilhermeht1906@gmail.com>
2022-01-13 16:28:11 -0300

    Início do editor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div>
-      Teste
-    </div>
-  );
+import { Component } from 'react';
+import MarkdownEditor from './markdown-editor';
+
+import 'normalize.css'
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      value: ''
+    }
+
+    this.handleChange = (e) => {
+      this.setState({
+        value: e.target.value
+      })
+    }
+
+    this.getMarkup = () => {
+      return { __html: this.state.value }
+    }
+  }
+
+  
+  
+  render() {
+    return (
+      <MarkdownEditor 
+        value={this.state.value} 
+        handleChange={this.handleChange} 
+        getMarkup={this.getMarkup}
+      />
+    );
+  }
 }
 
 export default App;
